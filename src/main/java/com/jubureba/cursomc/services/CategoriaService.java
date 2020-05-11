0af64ed71aeac210ea3a9fc4c9c0d0f9179b45e4
Anderson <eng.thiagolima@hotmail.com>
2020-05-11 13:53:51 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/jubureba/cursomc/services/CategoriaService.java
+++ src/main/java/com/jubureba/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jubureba.cursomc.domain.Categoria;
 import com.jubureba.cursomc.repositories.CategoriaRepository;
+import com.jubureba.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,18 @@ public class CategoriaService {
 	@Autowired //auto-instaciada pelo spring, pelo mecanismo de injecao de dependencia ou inversao de controle
 	private CategoriaRepository repo;
 	
-	
+	/*
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
+	}*/
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrando! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+
 }
