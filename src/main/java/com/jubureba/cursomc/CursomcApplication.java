6f0aae94e5eca637f8306872e65515829db2b30f
Anderson <eng.thiagolima@hotmail.com>
2020-05-11 11:43:14 -0300

    Criando operacao de instanciacao

--- src/main/java/com/jubureba/cursomc/CursomcApplication.java
+++ src/main/java/com/jubureba/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.jubureba.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jubureba.cursomc.domain.Categoria;
+import com.jubureba.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
